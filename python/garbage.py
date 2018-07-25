bb1a0984468038b147914cdf48aec36eec1ab304
kuchisriram <kuchisriram@gmail.com>
2018-07-25 11:20:24 +0530

    code updated to fix garbage collection flow.

--- python/garbage.py
+++ python/garbage.py
@@ -1,18 +1,19 @@
 import sys, gc
 
 def make_cycle():
-1 = { }
-1[0] = 1
+    x = { }
+    x[0] = x    
 
 def main():
-collected = gc.collect()
-print "Garbage collector: collected %d objects." % (collected)
-print "Creating cycles..."
-for i in range(10):
-make_cycle()
-collected = gc.collect()
-print "Garbage collector: collected %d objects." % (collected)
+    collected = gc.collect()
+    print "Garbage collector: collected %d objects." % (collected)
+    
+    print "Creating cycles..."
+    for i in range(10):
+        make_cycle()
+    collected = gc.collect()
+    print "Garbage collector: collected %d objects." % (collected)
 
 if __name__ == "__main__":
-ret = main()
-sys.exit(ret)
+    ret = main()
+    sys.exit(ret)
